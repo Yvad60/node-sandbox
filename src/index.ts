641cbf4cc39111510e8bda05502c9f72e9924ba7
Ivad <yvesyvad@gmail.com>
2023-09-09 08:55:04 +0200

    feat: add base express routes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,46 @@
-console.log("Hello world!");
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+const friends = [
+  {
+    id: 1,
+    name: "Ivad Yvess",
+  },
+  {
+    id: 2,
+    name: "Kate Nina",
+  },
+];
+
+app.listen(PORT, () => {
+  console.log("App listenning...");
+});
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} in ${delta}ms`);
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world I am an express app");
+});
+
+app.post("/friends", (req, res) => {
+  if (req.body.name == null) return res.status(400).json({ error: "Missing friend name" });
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length + 1,
+  };
+  friends.push(newFriend);
+  res.status(201).json(newFriend);
+});
+
+app.get("/friends", (req, res) => {
+  res.status(200).json(friends);
+});
